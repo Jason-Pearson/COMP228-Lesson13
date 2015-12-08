cc1035f444bca6cc595fc52ebeca0e8217960d01
Jason-Pearson <jason-pearson-1-0-7@hotmail.com>
2015-12-08 16:28:00 -0500

    Instantiated Two Threads via Thread Class, Showed Interleaving with Main

--- src/com/centennialcollege/comp228_lesson13/MyThread.java
+++ src/com/centennialcollege/comp228_lesson13/MyThread.java
@@ -2,15 +2,22 @@ package com.centennialcollege.comp228_lesson13;
 
 public class MyThread extends Thread { // class inherits Thread class, making this is a single thread 
 
+	//PRIVATE INSTANCE VARIABLES
+	private String _name;
+	
+	//CONSTRUCTOR
+	public MyThread(String name){ // get name of Thread object (a single thread) instantiated in Main Method
+		this._name = name;
+	}
 	
 	@Override
 	public void run() { // Run Method - runs this task every time it is called
 		//the task itself - a for loop
 		for(int index = 0; index < 10; index++)
 		{
-			System.out.println("Thread Count: " + index);
+			System.out.println("Thread " + this._name + " Count: " + index);
 			try { // after task is executed with no exception error
-				sleep(1000); // sleep has the task (to run the for loop) pause/wait for a set amount of milliseconds - 1000ms = 1s
+				sleep(1000); // sleep has the thread (task - to run the for loop) pause/wait for a set amount of milliseconds - 1000ms = 1s
 			} catch (InterruptedException e) {
 				System.err.println("Thread Interrupted: " + e);
 			}
--- src/com/centennialcollege/comp228_lesson13/Program.java
+++ src/com/centennialcollege/comp228_lesson13/Program.java
@@ -4,9 +4,24 @@ public class Program { // Driver Class
 
 	public static void main(String[] args) { // Main Method aka Main Process
 
-		MyThread  thread = new MyThread();
-		
-		thread.start(); // to execute a thread, run it's start method
+		MyThread  thread1 = new MyThread("1");
+		MyThread  thread2 = new MyThread("2");
+
+		thread1.start(); // (SECONDARY THREAD) to execute a thread, run it's start method
+		thread2.start(); // (SECONDARY THREAD) to execute a thread, run it's start method
+
+
+		//RUN THIS MAIN THREAD - THIS INTERLEAVES BETWEEN BOTH THREADS AND THEIR TASK (running both at the same time)
+		//MAIN THREAD TAKES PRIORITY
+		for(int index = 0; index < 10; index++)
+		{
+			System.out.println("Thread Count: " + index);
+			try { // after task is executed with no exception error
+				Thread.sleep(1000); //sleep (part of Thread class) - has the thread (task - to run the for loop) pause/wait for a set amount of milliseconds - 1000ms = 1s
+			} catch (InterruptedException e) {
+				System.err.println("Thread Interrupted: " + e);
+			}
+		}
 	}
 
 }
